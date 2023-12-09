3e4d688c34d2c797c1a971681053d6046c9125ac
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2023-12-08 19:28:36 -0600

    Se ha empezado el desarrollo del navbar de forma estatica, casi completado

--- app-frontend/src/components/nav-bar.jsx
+++ app-frontend/src/components/nav-bar.jsx
@@ -1,23 +1,49 @@
 import '../css/nav-Bar.css';
 
+import burguerIcon from '../assets/icons/burguer.svg';
+import createTaskIcon from '../assets/icons/Create_Task.svg';
+import createCollectionIcon from '../assets/icons/Create_Collection.svg';
+import deleteTasksOrCollectionsIcons from '../assets/icons/delete_tasks_or_collections.svg';
+
+
 export default function NavBar() {
     return (
         <nav>
-            <button>
-                <img src="../assets/icons/burguer.svg" alt="" />
-            </button>
-
-            <button>
-                <img src="../assets/icons/Create Task.svg" alt="" />
-            </button>
-
-            <button>
-                <img src="../assets/icons/Create Collection.svg" alt="" />
-            </button>
-
-            <button>
-                <img src="../assets/icons/delete tasks or collections.svg" alt="" />
-            </button>
+            <section id='options'>
+                <button><img src={burguerIcon} alt="burguer.svg" /></button>
+                <button><img src={createTaskIcon} alt="Create_Task.svg" /></button>
+                <button><img src={createCollectionIcon} alt="Create_Collection.svg" /></button>
+                <button><img src={deleteTasksOrCollectionsIcons} alt="delete_tasks_or_collections.svg" /></button>
+            </section>
+            <section id='collections'>
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+                <CollectionForNav />
+            </section>
         </nav>
+    )
+}
+
+function CollectionForNav() {
+    return (
+        <section className='collection'>
+            <hr/>
+            <h3>title for collection</h3>            
+        </section>
     )
 }
