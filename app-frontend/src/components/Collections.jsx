7f6c30177063162407872dbb9f79412be1b2f98e
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2023-12-09 23:08:02 -0600

    cambios siuuu

---
+++ app-frontend/src/components/Collections.jsx
@@ -0,0 +1,13 @@
+import '../css/Collections.css'
+
+export default function Collection() {
+    return(
+        <section className="Collection">
+            <div className='relleno'></div>
+            <div className='relleno'></div>
+            <div className='relleno'></div>
+            <div className='relleno'></div>
+            <h5>Title</h5>
+        </section>
+    )
+}
--- app-frontend/src/pages/renderAll.jsx
+++ app-frontend/src/pages/renderAll.jsx
@@ -1,42 +1,20 @@
 import Tasks from "../components/Tasks";
 import CollectionsOfTasks from "../components/CollectionsOfTasks";
+import Collection from "../components/Collections";
 
 import '../css/RenderAll.css';
 
 export default function RenderAll() {
     return (
         <section id="render-all">
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
-            <Tasks />
+            <Collection />
         </section>
+    )
+}
+
+function RenderTasks() {
+    
+    return (
+        <></>
     )
 }
