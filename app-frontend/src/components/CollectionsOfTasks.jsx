378cd66d11c5836629bced81c05de5c4ecb8ba01
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2023-12-09 19:13:58 -0600

    Pues ya casi quedaria la alfa del nuevo frontend para la app de to do list desarrollado en react en una rama distinta a la principal

---
+++ app-frontend/src/components/CollectionsOfTasks.jsx
@@ -0,0 +1,5 @@
+export default function CollectionsOfTasks() {
+    return (
+        <></>
+    )
+}
---
+++ app-frontend/src/pages/renderAll.jsx
@@ -0,0 +1,42 @@
+import Tasks from "../components/Tasks";
+import CollectionsOfTasks from "../components/CollectionsOfTasks";
+
+import '../css/RenderAll.css';
+
+export default function RenderAll() {
+    return (
+        <section id="render-all">
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+            <Tasks />
+        </section>
+    )
+}
