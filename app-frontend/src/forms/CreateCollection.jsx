29af3308ce8eda8b640099758689c8d28fc90bcc
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2023-12-18 20:02:37 -0600

    Finaliza el desarrollo de los esquemas de los formularios de manera estatica, proximanente se va a empezar a estlizar la pagina para dar empezar adar vida a la app

--- app-frontend/src/forms/CreateCollection.jsx
+++ app-frontend/src/forms/CreateCollection.jsx
@@ -13,47 +13,33 @@ export default function CreateCollection() {
         <section className="addTasksToCollectionOptional">
           <label htmlFor="Add Tasks">Add Tasks</label>
           <section className="listTasksForAdd">
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
-            <TaskForAdd />
+            <TaskForAdd id={0}/>
+            <TaskForAdd id={1}/>
+            <TaskForAdd id={2}/>
+            <TaskForAdd id={3}/>
+            <TaskForAdd id={4}/>
+            <TaskForAdd id={5}/>
+            <TaskForAdd id={6}/>
+            <TaskForAdd id={7}/>
+            <TaskForAdd id={8}/>
+            <TaskForAdd id={9}/>
+            <TaskForAdd id={9}/>
+            <TaskForAdd id={9}/>
+            <TaskForAdd id={3}/>
+            <TaskForAdd id={3}/>
+            <TaskForAdd id={3}/>
           </section>
         </section>
       </form>
+      <section className="Close-Form-Collection-Create"><p>x</p></section>
     </section>
   );
 }
 
-function TaskForAdd() {
+function TaskForAdd({id}) {
   return (
     <section className="taskForAdd">
-      <label htmlFor="task1">Title Task</label>
+      <label htmlFor="task1">Title Task, {id}</label>
       <input type="checkbox" name="task" id="" />
     </section>
   );
