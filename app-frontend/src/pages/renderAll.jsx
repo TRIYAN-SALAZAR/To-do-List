61b8b8cbfa2280c6c5e82349f3475bc7bd14a407
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2023-12-23 20:10:44 -0600

    Inicia la conexion de las peticiones del frontend hacia el backend GG

--- app-frontend/src/pages/renderAll.jsx
+++ app-frontend/src/pages/renderAll.jsx
@@ -4,9 +4,21 @@ import CreateCollection from "../forms/CreateCollectionForm";
 import CreateTask from "../forms/CreateTaskForm";
 import MessegeWarning from "../components/MessageWarning";
 
+import axios from 'axios';
+
 import "../css/RenderAll.css";
 
+function getTasksAndCollections() {
+  const data = axios.get('http://127.0.0.1:3000/')
+    .then(response => console.table(response.data))
+    .catch(err => console.log(err))
+
+    return (data);
+}
 export default function RenderAll() {
+
+  getTasksAndCollections()
+
   return (
     <>
       <section id="render-all">
