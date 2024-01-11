b769f47d09bdca464ca8f4cf0e3ff139c0c66605
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-01-11 17:03:46 -0600

    Cambios al mostrar la tarea de forma estatica

--- app-frontend/src/components/ShowTask.jsx
+++ app-frontend/src/components/ShowTask.jsx
@@ -1,9 +1,35 @@
-import '../css/ShowTask.css';
+import { useState } from "react";
 
-export default function ShowTask({id, setHiddenTask}) {
-    return (
-        <section className='container-main'>
+import "../css/ShowTask.css";
 
+export default function ShowTask({ id, setHiddenTask }) {
+  const [title, setTitle] = useState("Title of Task");
+
+  const handledTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  return (
+    <section className="container-main">
+      <form>
+        <label htmlFor="title-task"></label>
+        <input
+          type="text"
+          name="title-task"
+          id=""
+          value={title}
+          onChange={handledTitleChange}
+        />
+
+        <label htmlFor="description"></label>
+        <textarea name="description" id="">
+          This is a representation of description for task
+        </textarea>
+
+        <section className="btn-confirmar-o-cancelar">
+          <input type="submit" value="Confirmar cambios" />
+          <input type="buttom" value="Descartar cambios" />
         </section>
-    )
+      </form>
+    </section>
+  );
 }
