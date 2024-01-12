a8a2932e083a938d7fcc213bce4feec0573e98e3
Jorge Salazar <122687128+TRIYAN-SALAZAR@users.noreply.github.com>
2024-01-12 11:18:03 -0600

    correccion del bug al generar el componente de ShowTask

--- app-frontend/src/components/ShowTask.jsx
+++ app-frontend/src/components/ShowTask.jsx
@@ -5,24 +5,30 @@ import "../css/ShowTask.css";
 
 export default function ShowTask({ taskId, setHiddenTask }) {
   const [title, setTitle] = useState("Title of Task");
+  const [description, setDescription] = useState("");
   const [task, setTask] = useState({});
 
   const handledTitleChange = (event) => {
     setTitle(event.target.value);
   };
 
+  const handledDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
   useEffect(() => {
     const getTask = async () => {
-      const { task } = await axios.get("http://127.0.0.1:3000/tasks/" + taskId);
-      if (task === undefined) return;
-
-      setTask(task);
+      const { data } = await axios.get("http://127.0.0.1:3000/tasks/" + taskId);
+      if (data === undefined) return;
+      console.log(data);
+      setTask(data.task);
+      setTitle(data.task.title);
+      setDescription(data.task.description);
     };
 
     getTask();
-  }, []);
+  }, [taskId]);
 
-  console.log(task);
   return (
     <section className="container-main">
       <form>
@@ -36,13 +42,21 @@ export default function ShowTask({ taskId, setHiddenTask }) {
         />
 
         <label htmlFor="description"></label>
-        <textarea name="description" id="">
-          This is a representation of description for task
-        </textarea>
+        <textarea
+          name="description"
+          id=""
+          value={description}
+          onChange={handledDescriptionChange}
+        ></textarea>
 
         <section className="btn-confirmar-o-cancelar">
           <input type="buttom" value="Confirmar" className="confirm" />
-          <input type="buttom" value="cancelar" className="cancel" onClick={() => setHiddenTask(false)} />
+          <input
+            type="buttom"
+            value="cancelar"
+            className="cancel"
+            onClick={() => setHiddenTask(false)}
+          />
         </section>
       </form>
     </section>
--- app-frontend/src/components/Tasks.jsx
+++ app-frontend/src/components/Tasks.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import BtnEliminar from "./btn-eliminar";
-import ShowTask from './ShowTask';
+import ShowTask from "./ShowTask";
 
 import "../css/Tasks.css";
 
@@ -9,15 +9,17 @@ export default function Tasks({ title = "default", taskId }) {
   const [showTask, setShowTask] = useState(false);
 
   return (
-    <section
-      className="Task"
-      onMouseOver={() => setActive(true)}
-      onMouseLeave={() => setActive(false)}
-      onClick={() => setShowTask(true)}
-    >
-      <h5>{title}</h5>
-      {active && <BtnEliminar />}
-      {showTask && <ShowTask taskId={taskId} setHiddenTask={setShowTask}/>}
-    </section>
+    <>
+      <section
+        className="Task"
+        onMouseOver={() => setActive(true)}
+        onMouseLeave={() => setActive(false)}
+        onClick={() => setShowTask(true)}
+      >
+        <h5>{title}</h5>
+        {active && <BtnEliminar />}
+      </section>
+      {showTask && <ShowTask taskId={taskId} setHiddenTask={setShowTask} />}
+    </>
   );
 }
